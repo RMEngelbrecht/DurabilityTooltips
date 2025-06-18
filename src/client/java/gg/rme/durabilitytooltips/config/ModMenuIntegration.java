464ddb5763e305c445eff0c9f749104ab9628dc4
RME <ruben.m.engelbrecht@gmail.com>
2025-06-18 15:53:36 +0200

    fix: fixed crash when no visual config deps installed on 1.21.6

--- src/client/java/gg/rme/durabilitytooltips/config/ModMenuIntegration.java
+++ src/client/java/gg/rme/durabilitytooltips/config/ModMenuIntegration.java
@@ -3,19 +3,24 @@ package gg.rme.durabilitytooltips.config;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.MinecraftVersion;
 import net.minecraft.text.Text;
+
+import java.util.Objects;
 
 public class ModMenuIntegration implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        if (FabricLoader.getInstance().isModLoaded("yet_another_config_lib_v3")) {
-            return new YACLConfigScreen().create();
-        } else if (FabricLoader.getInstance().isModLoaded("cloth-config")) {
-            return new ClothConfigScreen().create();
+            if (FabricLoader.getInstance().isModLoaded("yet_another_config_lib_v3")) {
+                return new YACLConfigScreen().create();
+            } else if (FabricLoader.getInstance().isModLoaded("cloth-config")) {
+                return new ClothConfigScreen().create();
+            }
+        if (!Objects.equals(MinecraftVersion.CURRENT.getName(), "1.21.6")) {
+            return parent -> new InformationScreen(parent, Text.translatable("title.rmes-durability-tooltips.config"));
         }
-
-        return parent -> new InformationScreen(parent, Text.translatable("title.rmes-durability-tooltips.config"));
+        return null;
     }
 
 }
